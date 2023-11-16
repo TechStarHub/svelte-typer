b6ae8401bc46836011670e922a249367f9053994
iamBijoyKar <bijoypoco12345@gmail.com>
2023-11-16 20:11:42 +0530

    [UPDATE]: header ui updated

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,17 @@
 <script>
 </script>
 
-<header class="w-full px-6 py-2 " >
+<header class="w-full px-6 py-4 mb-8" >
     <a href="/" class="flex items-center gap-1">
-        <img src="/svelte.svg" alt="">
+        <img src="/svelte.svg" class="svelte-logo " alt="">
         <h1 class="text-2xl font-bold text-gray-800">
             Typer
         </h1>
     </a>
 </header>
+
+<style>
+    header>a:hover .svelte-logo {
+        filter: drop-shadow(0px 0px 7px #EC8F5E);
+    }
+</style>
