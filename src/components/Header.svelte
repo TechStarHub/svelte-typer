76056f13042836e5bf249ba3017a77e275009f95
iamBijoyKar <bijoypoco12345@gmail.com>
2023-11-16 19:13:24 +0530

    [UPDATE]: media updated

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,9 @@
 <script>
-
 </script>
 
 <header class="w-full px-6 py-2 " >
-    <a href="/" class="">
+    <a href="/" class="flex items-center gap-1">
+        <img src="/svelte.svg" alt="">
         <h1 class="text-2xl font-bold text-gray-800">
             Typer
         </h1>
